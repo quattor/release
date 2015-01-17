b04ff4ed80ff2832d5c47fb83b730a68f393bcc1
stdweird <stijn.deweirdt@ugent.be>
2015-01-17 18:58:27 +0100

    added yum based dependency installation (requires root for now)

--- src/scripts/build_all_repos.sh
+++ src/scripts/build_all_repos.sh
@@ -14,14 +14,6 @@ EOF
     exit 2
 }
 
-function error () {
-    # at least 2 arguments: exit code, the remainder is message
-    ec=$1
-    shift
-    echo $@
-    exit $ec
-}
-
 echo "$NAME START "`date +%s` `date`
 
 # ordered list of repository names
@@ -53,6 +45,35 @@ export QUATTOR_TEST_LOG_CMD_MISSING=1
 export QUATTOR_TEST_LOG_CMD=1
 PROVEARGS="-Dprove.args=-v"
 
+# Dependencies checking
+# set to 0 on non-yum systems
+CHECKDEPS=1
+
+# Binary dependencies to be installed with yum                                                                                                        
+DEPS_INIT_BIN_YUM="rpmbuild perl panc mvn git"
+
+# the mvn epel url (who has this mirrored/enabled by default?)                                                                                        
+EPEL_MVN_REPO=https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo
+
+function error () {
+    # at least 2 arguments: exit code, the remainder is message
+    ec=$1
+    shift
+    echo $@
+    exit $ec
+}
+
+function cerror () {
+    # conditional error                                                                                                                               
+    fatal=$1
+    shift
+    if [ $fatal -gt 0 ]; then
+        error $@
+    else
+        echo $@
+    fi
+}
+
 function get_cpanm () {
     which curl >& /dev/null
     if [ $? -gt 0 ]; then
@@ -73,6 +94,190 @@ function get_cpanm () {
     fi
     
     return 0
+}
+
+function has_mvn () {
+    mvn="mvn"
+    # this one is typically missing from repos etc etc
+    which $mvn >& /dev/null
+    if [ $? -gt 0 ]; then
+        echo "No maven executable $mvn found in PATH"
+        
+        deps_install_yum "*bin/mvn" 0
+        if [ $? -gt 0 ]; then
+            fn=/etc/yum.repos.d/check_deps_mvn.repo
+            echo "Couldn't get mvn $mvn via yum. Going to add the mvn epel repo $EPEL_MVN_REPO to $fn and retry."
+
+            which curl >& /dev/null
+            if [ $? -gt 0 ]; then
+                which wget >& /dev/null
+                if [ $? -gt 0 ]; then
+                    error 80 "has_mvn no curl or wget found"
+                else
+                    wget -O $fn "$EPEL_MVN_REPO"
+                fi
+            else
+                curl -o $fn "$EPEL_MVN_REPO"
+            fi
+
+            # releasever but repos have single digits/RHEL naming
+            major=`cat /etc/redhat-release | sed -n "s/.*release \([0-9]\+\)\..*/\1/p"`
+            if [ -z "$major" ]; then
+                error 81 "No major release version found via /etc/redhat-release"
+            fi
+            echo "Going to use releasever $major for this repo"
+            sed -i "s/\$releasever/$major/g" $fn
+
+            if [ $? -gt 0 ]; then
+                error 82 "has_mvn fetch mvn epel repo $EPEL_MVN_REPO failed" 
+            fi
+            
+            # now it's fatal
+            deps_install_yum "*bin/mvn" 1
+        fi
+    fi
+
+    return 0
+}
+
+function check_deps_minimal () {
+    for pkg in which rpm yum repoquery; do
+        which $pkg >& /dev/null
+        if [ $? -gt 0 ]; then
+            error 60 "Failed minimal dependency $pkg"
+        fi
+    done
+}
+
+function deps_install_yum () {
+    dep="$1"
+    # default fatal
+    fatal=${2:-1}
+
+    ec=0
+
+    echo "Searching for dep $dep"
+    pkgs=`repoquery --qf '%{name}' --whatprovides "$dep" | sort| uniq`
+    if [ -z "$pkgs" ]; then
+        ec=70
+        cerror $fatal $ec "No packages found for dep $dep with repoquery"
+    fi
+
+    # e.g. quattor unittest repo
+    enablerepos=""
+
+    # yum command (e.g. add sudo or something like that)
+    yum="yum"
+
+    for pkg in $pkgs; do
+        echo "Installing pkg $pkg with yum"
+        cmd="$yum install -y $enablerepos $pkg"
+        $cmd
+        if [ $? -gt 0 ]; then
+            ec=71
+            cerror $fatal $ec "Failed installation of pkg $pkg with yum : $cmd"
+        fi
+    done
+
+    return $ec
+}
+
+function check_deps_init_bin () {
+    echo "Checking DEPS_INIT_BIN_YUM $DEPS_INIT_BIN_YUM"
+
+    # do it separately
+    has_mvn
+
+    for bin in $DEPS_INIT_BIN; do
+        # these are fatal
+        deps_install_yum "*bin/$bin" 1
+    done
+
+    # should be part of rpmbuild dep
+    deps_install_yum /usr/lib/rpm/find-requires 1
+
+    echo "Done checking DEPS_INIT_BIN $DEPS_INIT_BIN"    
+    return 0
+}
+
+function get_cpan_dep () {
+    perlpkg="$1"
+    
+    echo "Looking for CPAN perlpkg $perlpkg"
+    
+    # TODO actually implement this
+    CPAN="$CPAN $perlpkg"
+}
+
+function get_repo_deps () {
+    repo=$1
+    
+    cd $REPOSITORY
+
+    # look for src subdirs (like AII or components)
+    subdirs=`find $repo -type d -name src`
+
+    echo "Found subdirs for repository $repo : $subdirs"
+    
+    for dir in $subdirs; do
+        cd $REPOSITORY
+        get_repo_deps_subdir $repo $dir
+    done
+}
+
+function get_repo_deps_subdir () {
+    repo=$1
+
+    # repo + optional subdir
+    dir=${2:-$repo}
+
+    # no src or target, just search the whole repo/subdir
+    cd $dir
+
+    origIFS="$IFS"
+    # newline is th eonly delimiter for IFS
+    fakeIFS="
+"
+    export IFS=$fakeIFS
+    deps=`find . -type f | /usr/lib/rpm/find-requires | sort | uniq`
+
+    echo "Deps found for repo $repo: $deps"
+
+    # WARNING dep can have whitespace!
+    for dep in $deps; do
+        export IFS=$origIFS 
+        echo "Checking dependency '$dep'"
+        perlpkg=`echo $dep | sed -n "s/^perl(\(.*\))\(.*\(\s[0-9]\+.*\)\)\?$/\1\3/p"`
+        if [ -z "$perlpkg" ]; then
+            echo "Dependency $dep is not a perl package"
+            # is it a full path?
+            if [[ "$dep" == /* ]] && [ -f "$dep" ]; then
+                echo "Dependency $dep is a absolute filename and exists"
+            else
+                deps_install_yum "$dep" 1
+            fi
+        else
+            # Test if is usable
+            echo "Dependency $dep is a perl package"
+            # The unittests run prove with additional paths
+            PERL5LIB="$PERL5LIB:src/test/perl:src/test/resources" perl -e "use $perlpkg;" >& /dev/null
+            if [ $? -eq 0 ]; then
+                echo "Dependency $dep is a usable perl package"                
+            else 
+                # try to get it with yum
+                deps_install_yum "$dep" 0                
+                if [ $? -eq 0 ]; then
+                    echo "Dependency $dep is a perl package found with yum"                
+                else
+                    get_cpan_dep "$dep"
+                fi
+            fi
+        fi
+        # Is thisone really necessary?
+        export IFS=$fakeIFS
+    done
+    
+    export IFS=$origIFS 
 }
 
 function reset_perl5lib () {
@@ -217,8 +422,6 @@ function build_and_install () {
     return 0
 }
 
-
-
 function main() {
     mkdir -p $DEST
     if [ $? -gt 0 ]; then
@@ -251,6 +454,28 @@ function main() {
 	        error 7 "build_and_install compile of repository $repo failed"
 	    fi
     done
+
+    # check dependencies
+    if [ $CHECKDEPS -gt 0 ]; then
+
+        echo "Checking dependencies"
+
+        check_deps_minimal
+        check_deps_init_bin
+
+        for repo in $REPOS_MVN_ORDERED; do
+            get_repo_deps $repo
+            if [ $? -gt 0 ]; then
+                error 9 "check_deps of repository $repo failed"
+            fi
+        done
+
+        # TODO needs to be implemented. for now we gather the deps
+        if [ ! -z "$CPAN" ]; then
+            # fatal for now?
+            error 9 "Missing CPAN packages $CPAN"
+        fi
+    fi
 
     # with modified PERL5LIB, run the tests
     for repo in $REPOS_MVN_ORDERED; do
