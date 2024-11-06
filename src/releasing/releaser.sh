ff49f772ba18631abbc33e3f5b99c19acde233c7
James Adams <james.adams@stfc.ac.uk>
2024-11-06 16:16:43 +0000

    releasing: prevent globbing and word splitting of VERSION

--- src/releasing/releaser.sh
+++ src/releasing/releaser.sh
@@ -266,7 +266,7 @@ if gpg-agent; then
             for r in $REPOS_MVN; do
                 echo_info "---------------- Releasing $r ----------------"
                 cd $r
-                mvn -e -q -DautoVersionSubmodules=true -Dgpg.useagent=true -Darguments=-Dgpg.useagent=true -B -DreleaseVersion=$VERSION clean release:prepare
+                mvn -e -q -DautoVersionSubmodules=true -Dgpg.useagent=true -Darguments=-Dgpg.useagent=true -B -DreleaseVersion="$VERSION" clean release:prepare
                 if [[ $? -gt 0 ]]; then
                     echo_error "RELEASE FAILURE"
                     exit 1
